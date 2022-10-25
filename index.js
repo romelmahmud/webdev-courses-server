249bfc434509be24cfca5c5577cf9a9365f7e919
Romel Mahmud <romelmahmud.dev@gmail.com>
2022-10-25 21:34:39 +0600

    categories data added and route set up complete

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const port = 5000;
 
+const categories = require("./db/categories.json");
+
 const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome to home page");
+});
+
+app.get("/categories", (req, res) => {
+  res.send(categories);
 });
 
 app.listen(port, () => {
