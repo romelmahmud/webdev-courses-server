f90307395011b375e811a59a45ff60f59d305efc
Romel Mahmud <romelmahmud.dev@gmail.com>
2022-10-25 21:20:55 +0600

    server setup complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+const port = 5000;
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to home page");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
